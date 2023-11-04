5ece659a41e6431ac6d08bdd014285527df479fb
onifernando1 <onifernando1@gmail.com>
2023-11-04 21:49:12 +0000

    changed bio

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -10,61 +10,55 @@ function About() {
       ></img>
       <div className="about-text">
         <p>
-          I have always loved drawing and painting for as long as I can
-          remember. I spent all my childhood creating.{" "}
-        </p>{" "}
-        <p>
-          Coming from an Asian background, art was not a career pathway that was
-          encouraged and so I ended up in Glasgow, where I studied Dentistry.{" "}
-          <p>It was here that my passion for the arts was re-kindled. </p>A
-          close friend failed her first year at dental school and decided to
-          change to architecture so needed to build up her portfolio...I joined
-          an evening art class with her at Glasgow School of Art, where my
-          journey began.{" "}
-          <p>
-            Returning to London, I worked as a dentist full-time initially,
-            eventually going to part time as I felt compelled to paint.
-          </p>
-          <p>
-            In 2008 I, sadly lost my husband unexpectedly. I was left with 3
-            young children. It was this life-changing event that confirmed for
-            me that life is too short not to follow your heart and ever since I
-            have committed to pursuing my art as a career.
-          </p>{" "}
-          <p>
-            {" "}
-            I am self-taught, which I feel has been a blessing as I follow no
-            learnt rules and I hold no expectations. I paint from the heart,
-            experimenting with new styles and techniques.{" "}
-          </p>{" "}
-          <p>
-            Life is my inspiration. I am fascinated by people and emotions and I
-            spend alot of time in nature, in Richmond park and by the water,
-            both the sea and the river. As a result, my art is eclectic. I love
-            painting figures and faces, animals, sea and landscape and abstract.{" "}
-          </p>{" "}
-          <p>
-            My style varies too, depending on my mood. I love working in oils
-            with knives applied thickly creating texture and form using vibrant
-            rich colours but also love the delicate impressionistic washes of
-            pen, ink and watercolor.{" "}
-          </p>{" "}
-          <p>
-            I love a commission, as I recognise that each one that I have had
-            has pushed me to grow as an artist as it challenges me to do
-            something that I would not otherwise have possibly tried.
-          </p>{" "}
-          <p>
-            I have recently started running art workshops from my garden studio
-            for both children and adults.
-          </p>{" "}
-          <p>
-            {" "}
-            I also work as a craniosacral therapist which I feel enhances my art
-            as it is a meditative grounding practice that enables you to get out
-            of your own way and feel.
-          </p>
+          {" "}
+          Being almost entirely self-taught has proven to be a blessing, freeing
+          me from conventional rules and expectations.{" "}
         </p>
+        <p>
+          {" "}
+          Although innately drawn to art, I grew up in a family where art as a
+          career was not encouraged. So I found myself at art evening classes at
+          the Glasgow School of Art alongside my daytime dental course.{" "}
+        </p>
+        <p>
+          However after qualifying and working as a dentist back in London, in
+          2008, the sudden and unexpected loss of Chris, my husband, completely
+          changed the course of my life.
+        </p>
+        <p>
+          Leaving me a single parent to three children, it was a profound wake
+          up call that life is short.
+        </p>
+        <p>
+          {" "}
+          Fifteen years on, I run a flourishing art business, selling both off
+          the shelf and bespoke pieces of art. My art is an expression of my
+          heart, often taking inspiration from life itself. People, emotions,
+          nature-particularly the countryside, the river and the coast -all fuel
+          my eclectic creations. My artistic style is as varied as my ranges
+          from vibrant oil paintings with textured knife work to delicate
+          monochromatic impressions in pen and ink and watercolor.{" "}
+        </p>
+        <p>
+          {" "}
+          Commissions hold a special place in my artistic journey, pushing me
+          beyond my comfort zone and encouraging growth.
+        </p>
+        <p>
+          More recently, the art workshops held in my garden studio, for both
+          adults and younger people, have proved a life-affirming extension to
+          my work. People from all walks of life, ages, backgrounds and cultures
+          gather in small groups to uncover their own artistic creativity - for
+          some it is an astonishing discovery! In addition to all this, I also
+          practice craniosacral therapy, a meditative grounding technique which
+          complements and enriches my creative process.{" "}
+        </p>
+        <p> Thank you for looking at my art, I hope you enjoy it. </p>
+        <p>
+          Please feel free to contact me with any questions. I look forward to
+          hearing from you.
+        </p>
+        <p></p>
       </div>
     </div>
   );
